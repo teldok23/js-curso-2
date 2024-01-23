b8b31f7cb7b3b1556581f717341460455c3183b7
teldok23 <data.teldo820@gmail.com>
2024-01-23 01:28:35 -0500

    avance

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora del Desafío';
 
+function botononConsole(){
+    console.log('El boton fue clicado');
+}
+
+function botononPrompt(){
+    let ciudad = prompt('Dime una ciudad de Varsil');
+    alert('Estuve en ' + ciudad + ' y me acordé de ti.');
+}
+
+function botononJs(){
+    alert('Yo amo JS');
+}
+
+function botononSuma(){
+    let sum1 = prompt('Yo amo JS');
+}
